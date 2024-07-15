fd00603a459f4b970e5297ba4a15764020f4929e
Lavender <anyangolavenderr@gmail.com>
2024-07-15 14:51:36 +0300

    creating apis

--- school/course/models.py
+++ school/course/models.py
@@ -1,19 +1,15 @@
 from django.db import models
+from teacher.models import Teacher
 
-# Create your models here.
 class Course(models.Model):
-     course_name = models.CharField(max_length = 20)
-     course_objective = models.CharField(max_length = 20)
-     course_duration = models.DurationField()
-     course_students = models.CharField(max_length = 20)
-     course_level = models.PositiveSmallIntegerField()
-     course_description= models.TextField(max_length = 30)
-     course_title = models.CharField(max_length = 10)
-     course_teacher = models.CharField(max_length = 15)
-     course_id = models.PositiveSmallIntegerField()
-     course_resources = models.CharField(max_length = 19)
-
-
-
-     def __str__(self):
-        return f"{self.course_name} {self.course_level}"
+    course_title = models.CharField(max_length=20)
+    course_category = models.CharField(max_length=20)
+    course_start_date = models.DateField()
+    course_end_date = models.DateField()
+    course_code = models.PositiveSmallIntegerField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='courses')
+    maximum_attendees = models.PositiveSmallIntegerField()
+    course_level = models.CharField(max_length=20)
+    course_fee = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return f"{self.course_title} ({self.course_category})"
--- school/student/models.py
+++ school/student/models.py
@@ -1,22 +1,20 @@
 from django.db import models
-
-# Create your models here.
+from classroom.models import Classroom 
+from course.models import Course
 
 class Student(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
-    code = models.PositiveSmallIntegerField()
+    student_code = models.PositiveSmallIntegerField()
     date_of_birth = models.DateField()
     country = models.CharField(max_length=20)
     bio = models.TextField()
-    phone_number = models.CharField()
+    age = models.PositiveSmallIntegerField()
+    phone_number = models.CharField(max_length=20)
     immediate_contact = models.CharField(max_length=20)
-    pic = models.ImageField()
-
-
-
+    image = models.ImageField()
+    classes = models.ManyToManyField(Classroom, related_name='students')
+    courses = models.ManyToManyField(Course, related_name='students')
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-        
-
+        return f"{self.first_name} {self.last_name}"
